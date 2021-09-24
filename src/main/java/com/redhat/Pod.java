2ead93973ed7739838803920118d6815d99deedf
Carlos Simon <casimon@redhat.com>
2021-09-24 13:28:59 -0500

    runtime, exclude self

--- src/main/java/com/redhat/Pod.java
+++ src/main/java/com/redhat/Pod.java
@@ -3,8 +3,10 @@ package com.redhat;
 public class Pod {
   public String name;
   public boolean isRunning;
-  public Pod(String name, boolean isRunning) {
+  public String runtime;
+  public Pod(String name, boolean isRunning, String runtime) {
     this.name = name;
     this.isRunning = isRunning;
+    this.runtime = runtime;
   }
 }
--- src/main/java/com/redhat/SmasherResource.java
+++ src/main/java/com/redhat/SmasherResource.java
@@ -9,11 +9,16 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 
 @Path("/")
 public class SmasherResource {
+
+    @ConfigProperty(name="self.tag", defaultValue = "smasher-back")
+    String self_tag;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -22,7 +27,13 @@ public class SmasherResource {
         List<Pod> pods = new ArrayList<>();
         try {
             k8s = new DefaultKubernetesClient();
-            pods = k8s.pods().list().getItems().stream().map(x -> new Pod(x.getMetadata().getName(), false))
+            pods = k8s.pods().withoutLabel("app", self_tag).list().getItems().stream()
+                    .map(x -> new Pod(
+                                x.getMetadata().getName(),
+                                x.getStatus().getPhase().equals("Running"),
+                                x.getMetadata().getAnnotations().get("app.openshift.io/runtime")
+                                )
+                            )
                     .collect(Collectors.toList());
         } catch (Exception ex) {
             ex.printStackTrace();
